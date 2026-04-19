50dc47761c480f9d57a4ada70a2381c79c402240
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-04-19 21:55:01 +0530

    main setup is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fintraq/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,14 +6,22 @@ void main() {
 }
 
 class FintraqApp extends StatelessWidget {
-  const FintraqApp({super.key})
+  const FintraqApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fintraq',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        fontFamily: 'DM Sans',
+        colorScheme: ColorScheme.dark(
+          primary: AppColors.green,
+          surface: AppColors.surface,
+          background: AppColors.background,
+        ),
+      ),
     );
   }
 }
